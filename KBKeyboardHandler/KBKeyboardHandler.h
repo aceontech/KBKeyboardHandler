3027ec7406fa4886bede37a6ea7e7d53e01d5a0a
Alex Manarpies <alex@manarpies.com>
2013-10-17 14:43:40 +0200

    Init

---
+++ KBKeyboardHandler/KBKeyboardHandler.h
@@ -0,0 +1,13 @@
+//
+//  KBKeyboardHandler.h
+//  KBKeyboardHandler
+//
+//  Created by Alex Manarpies on 17/10/13.
+//  Copyright (c) 2013 Alex Manarpies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KBKeyboardHandler : NSObject
+
+@end
---
+++ KBKeyboardHandler/KBKeyboardHandler.m
@@ -0,0 +1,13 @@
+//
+//  KBKeyboardHandler.m
+//  KBKeyboardHandler
+//
+//  Created by Alex Manarpies on 17/10/13.
+//  Copyright (c) 2013 Alex Manarpies. All rights reserved.
+//
+
+#import "KBKeyboardHandler.h"
+
+@implementation KBKeyboardHandler
+
+@end
